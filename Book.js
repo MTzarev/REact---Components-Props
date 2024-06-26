021dcac671a5b6e386b97152c854633ec19d7433
Miroslav Tsarev <mtzarev@gmail.com>
2024-06-26 22:21:25 +0300

    Add files via upload

---
+++ Book.js
@@ -0,0 +1,7 @@
+
+const Book = (props)=> {
+    return (
+        <li>{props.text}</li>
+    )
+}
+export default Book
---
+++ BookList.js
@@ -0,0 +1,12 @@
+import Book from './Book.js'
+function BookList (){
+    return(
+        <ol>
+            <BookList text = "Stephan King"/>
+            <BookList text = "Gosho Goshev"/>
+            <BookList text = "Dragan Draganov"/>
+          
+        </ol>
+    )
+}
+export default BookList
